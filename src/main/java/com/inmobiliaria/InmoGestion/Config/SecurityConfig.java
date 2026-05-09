0d023424104ffccf2fad2ea7dec8617f37e04c5f
Lucas Caro <lucascaro97@gmail.com>
2026-05-09 15:17:14 -0300

    add vercel url cors

--- src/main/java/com/inmobiliaria/InmoGestion/Config/SecurityConfig.java
+++ src/main/java/com/inmobiliaria/InmoGestion/Config/SecurityConfig.java
@@ -53,7 +53,7 @@ public class SecurityConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
-        corsConfiguration.setAllowedOrigins(List.of("http://localhost:5173"));
+        corsConfiguration.setAllowedOrigins(List.of("http://localhost:5173", "https://inmogestion-cliente.vercel.app/login"));
         corsConfiguration.addAllowedHeader("*");
         corsConfiguration.addAllowedMethod("*");
         corsConfiguration.setAllowCredentials(true);
